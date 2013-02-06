f111ce724403d4264796b126ab4934ff0f28e7bb
Ben Straub <straub@auburn.edu>
2013-02-06 14:55:42 -0600

    Added some support functions for Drive.ino.

--- RobotCode/MobileUnit/Drive.ino
+++ RobotCode/MobileUnit/Drive.ino
@@ -2,6 +2,9 @@
  * This file will contain the driveTurn and driveStraight functions.
  */
 
+//Some method declarations:
+double error(location target, location current);
+double dist(location a, location b);
  
 int driveTurn(double newTheta, boolean useLines) {
   return 0;
@@ -9,4 +12,16 @@ int driveTurn(double newTheta, boolean useLines) {
 
 int driveStraight(location target, boolean useLines) {
   return 0;
+}
+
+double error(location target, location start, location current) {
+  return (((current.x-start.x)*(target.y-start.y))-((current.y-start.y)*(target.x-start)))/dist(start, target);
+}
+
+double dist(location a, location b) {
+  return sqrt(((b.y-a.y)*(b.y-a.y)) - ((b.x-a.x)*(b.x-a.x)));
+}
+
+double arcdist(double theta1, double theta2, double radius) {
+  return (theta2 - theta1)*radius;
 }
