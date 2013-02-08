4ba095b70c273e4549abe8769763707d8f16fd8d
Ben Straub <straub@auburn.edu>
2013-02-08 16:59:41 -0600

    Put in the route calculation math.
    Some of this may need to go somewhere else (in a different file).  I
    wasn't sure where to put it.

--- RobotCode/MobileUnit/CommandConversion.ino
+++ RobotCode/MobileUnit/CommandConversion.ino
@@ -17,6 +17,14 @@
  * Exception: If destination is within turning diameter of current location,
  * use two pivots and one straight move.
  */
+ 
+#define HALF_WIDTH 5  // !!! should probably go somewhere more universal
+ 
+//method declaration
+location calcWaypoint(location A, location B, signed char& dir); //may need to add some flags
+double adjustTheta( double theta ); // !!! should probably go somewhere more universal
+
+
 byte commandConversion() {
   motorPath[0] = M_FORWARD_RIGHT;
   motorPath[1] = M_FORWARD;
@@ -31,4 +39,24 @@ byte commandConversion() {
   partTwoDest.y = 20000;
   partTwoDest.theta = 0;
   return 0;
+}
+
+// return.theta gives the 
+location calcWaypoint(location A, location B, signed char& dir) {
+  location wp; //waypoint to be returned
+  dir = signbit(adjustTheta(atan2((B.y-A.y),(B.x-A.x))-A.theta));
+  wp.theta = atan2( (B.y-A.y+HALF_WIDTH*(cos(B.theta)-cos(A.theta))*dir),
+                    (B.x-A.x-HALF_WIDTH*(sin(B.theta)-sin(A.theta))*dir));
+  wp.x = B.x - dir*HALF_WIDTH*(sin(B.theta)-sin(wp.theta));
+  wp.y = B.y + dir*HALF_WDITH*(cos(B.theta)-cos(wp.theta));  
+  return wp;
+}
+
+double adjustTheta( double theta )
+{
+  while (theta > PI)
+    theta -= PI;
+  while (theta < -PI)
+    theta += PI;
+  return theta;
 }
