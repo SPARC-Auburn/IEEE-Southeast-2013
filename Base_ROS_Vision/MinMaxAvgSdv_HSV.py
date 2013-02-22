6c983e87fb9a1fe6eb9462e236a7d90241be1369
Patrick Berry <peanutbutter802@gmail.com>
2013-02-21 20:30:11 -0600

    Added Folder For ROS, BaseStation, and Vision code

---
+++ Base_ROS_Vision/MinMaxAvgSdv_HSV.py
@@ -0,0 +1,72 @@
+## Author: Patrick Berry
+## Purpose: Find the Mininum, Maximum, Average, Standard Deviation of the HueSatVal channels given a list of images in a file
+## Last Updated 2013-02-21
+## 
+
+## Cats
+
+from cv import SetImageCOI,MinMaxLoc,LoadImage,AvgSdv,CV_BGR2HSV,CvtColor
+from sys import argv
+
+def main():
+	## CHecks to make sure there is the correct number of command line arguments
+	if( argv.__len__() != 2):
+		print "\nError\nUsage: MinMaxAvgSdv_HSV.py fileList.txt\n"
+		return -1
+	####
+
+	## Opens the filelist and reads in all the flieNames
+	fileHandle = open(argv[1],'r')
+	fileList = [line.strip() for line in fileHandle]
+	fileHandle.close()
+	
+	print "\n\nMin/Max/Avg/Sdv for HSV values for images found in:\n",argv[1]
+	for fileName in fileList:
+
+		## ignores any blank lines
+		if( fileName == '' ):
+			continue
+		####
+
+		## Opens the image
+		img = LoadImage(fileName)
+
+		## Converts the image from BlueGreenRed to HueSaturationValue
+		CvtColor(img,img,CV_BGR2HSV)
+
+		## Lists that hold values for each channel
+		min = []
+		max = []
+		avg = []
+		sdv = []
+
+		#Loop through all the channels. Ignores ch4/alpha channel
+		for i in xrange(3):
+			## Sets ChannelOfInterest 
+			## COI starts at 1, (0 means no COI)
+			SetImageCOI(img,i+1)
+
+			(tempMin,tempMax,minLoc,maxLoc) = MinMaxLoc(img)
+			min.append(tempMin)
+			max.append(tempMax)
+			(tempAvg,tempSdv) = AvgSdv(img)
+			## AvgSdv returns a list with two cvScalars [avg,sdv]
+			## but since the COI is set only the first one contains any
+			## useful information
+			avg.append(tempAvg[0])
+			sdv.append(tempSdv[0])
+		####
+
+		print "\n",fileName
+		print "\tMin:\t",min
+		print "\tMax:\t",max
+		print "\tAvg:\t",avg
+		print "\tSdv:\t",sdv
+	####
+	print "\n\n"
+####
+
+
+if __name__=="__main__":
+	main()
+####
