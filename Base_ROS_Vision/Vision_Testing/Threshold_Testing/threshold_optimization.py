ecbd6e6b7bf9c2b360036b4a0f15749df638c19f
patrickBerry <peanutbutter802@gmail.com>
2013-03-14 02:42:53 -0500

    Finished optimization script, commit-ting before running it all night.

--- Base_ROS_Vision/Vision_Testing/Threshold_Testing/threshold_optimization.py
+++ Base_ROS_Vision/Vision_Testing/Threshold_Testing/threshold_optimization.py
@@ -36,11 +36,11 @@ class BlockFinder():
 								color = (0,0,0), \
 								lowerb = (140,  0, 41), \
 								upperb = (125, 39, 61) )
-		self.red1_hsv = Threshold( name = "red1", \
+		self.red1_hsv = Threshold( name = "red", \
 								color = (0,0,255), \
 								lowerb = (  0,165, 61), \
 								upperb = (  6,212,126) )
-		self.red2_hsv = Threshold( name = "red2", \
+		self.red2_hsv = Threshold( name = "red", \
 								color = (0,0,255), \
 								lowerb = (169,165, 61), \
 								upperb = (179,212,126) )
@@ -116,7 +116,7 @@ class BlockFinder():
 
 		(workingdir,exec_ext) = path.split(path.realpath(__file__))
 		self.curtim = int(time.time())
-		self.workingdir = workingdir+"/"
+		self.workingdir = workingdir+"/optimization_test_images/"
 
 		self.original = cv2.imread(self.workingdir+"original.png")
 
@@ -131,8 +131,8 @@ class BlockFinder():
 
 		self.watch = False
 
-		self.SIGMA = 8
-		self.N = 150
+		self.SIGMA = 1
+		self.N = 20
 		self.MAX_ITER = 5000
 		self.MIN_FITNESS = 0
 
@@ -142,22 +142,32 @@ class BlockFinder():
 
 		##Initializations
 		ycrcb_img = cv2.cvtColor(self.original,cv2.COLOR_BGR2YCR_CB)
+		hsv_img = cv2.cvtColor(self.original,cv2.COLOR_BGR2HSV)
 		outputFile = open(self.workingdir+"Threshold_Optimization_Test_"+str(self.curtim)+".txt",'w')
+
+		hsv_list_minus_reds =[ self.black_hsv, \
+							self.orange_hsv, \
+							self.yellow_hsv, \
+							self.green_hsv, \
+							self.blue_hsv, \
+							self.brown_hsv ]
 		try:
+			outputFile.write("YCR_CB Optimization\n")
+
 			for initial_seed in self.ycrcb_thresholds:
 				iterations = 0
 				stale_gen = 0
 				seed = initial_seed
-
+				sigma = self.SIGMA
 				##Best initializations...
 				thr_img = cv2.inRange(src = ycrcb_img, \
 										lowerb = seed.lowerb, \
 										upperb = seed.upperb )
 				best_score = self.findFitness(thr_img,self.correct_id[seed.name])
 				best_child = seed
-				outputFile.write("\n\nSeed\t"+str(best_score)+"\t"+str(best_child))
+				outputFile.write("\n\nSeed\t"+str(best_score)+"\t"+str(best_child)+"\n")
 				while( iterations < self.MAX_ITER and best_score >= self.MIN_FITNESS ):
-					children = self.generateThresholdValues(seed)
+					children = self.generateThresholdValues(seed,sigma)
 					fitness_scores = []
 					for (i,child) in enumerate(children):
 						thr_img = cv2.inRange(src = ycrcb_img, \
@@ -174,13 +184,13 @@ class BlockFinder():
 						best_child = seed
 						seed = children[index]
 						stale_gen = 0
-						outputFile.write(str(iterations)+"\t"+str(gen_score)+"\t"+str(seed))
+						outputFile.write(str(iterations)+"\t"+str(gen_score)+"\t"+str(seed)+"\n")
 					else:
 						stale_gen = stale_gen + 1
 
 						if( stale_gen > 20 ):
-							self.SIGMA = self.SIGMA + 1
-							outputFile.write("Stale generation:\t"+str(i)+"\tsigma: "+str(self.SIGMA))
+							sigma = sigma + 1
+							outputFile.write("Stale generation:\t"+str(iterations)+"\tsigma: "+str(sigma)+"\n")
 							stale_gen = 0
 						####					
 					####
@@ -188,8 +198,124 @@ class BlockFinder():
 						cv2.waitKey(100)
 					####
 				####
-				outputFile.write("\nBEST!\nscore: "+str(best_score)+"\tthreshold: "+str(best_child))
+				outputFile.write("BEST!\nscore: "+str(best_score)+"\tthreshold: "+str(best_child)+"\n")
 			####
+
+			outputFile.write("\n\nHSV Optimization\n")
+			
+			for initial_seed in hsv_list_minus_reds:
+				iterations = 0
+				stale_gen = 0
+				seed = initial_seed
+				sigma = self.SIGMA
+				##Best initializations...
+				thr_img = cv2.inRange(src = ycrcb_img, \
+										lowerb = seed.lowerb, \
+										upperb = seed.upperb )
+				best_score = self.findFitness(thr_img,self.correct_id[seed.name])
+				best_child = seed
+				outputFile.write("\n\nSeed\t"+str(best_score)+"\t"+str(best_child)+"\n")
+				while( iterations < self.MAX_ITER and best_score >= self.MIN_FITNESS ):
+					children = self.generateThresholdValues(seed,sigma)
+					fitness_scores = []
+					for (i,child) in enumerate(children):
+						thr_img = cv2.inRange(src = ycrcb_img, \
+												lowerb = child.lowerb, \
+												upperb = child.upperb )
+						fitness = self.findFitness(thr_img,self.correct_id[child.name])
+						fitness_scores.append( (fitness,i) )
+					####
+					(gen_score,index) = min(fitness_scores)
+					iterations = iterations +1
+
+					if( gen_score < best_score ):
+						best_score = gen_score
+						best_child = seed
+						seed = children[index]
+						stale_gen = 0
+						outputFile.write(str(iterations)+"\t"+str(gen_score)+"\t"+str(seed)+"\n")
+					else:
+						stale_gen = stale_gen + 1
+
+						if( stale_gen > 20 ):
+							sigma = sigma + 1
+							outputFile.write("Stale generation:\t"+str(iterations)+"\tsigma: "+str(sigma)+"\n")
+							stale_gen = 0
+						####					
+					####
+					if( self.watch == True ):
+						cv2.waitKey(100)
+					####
+				####
+				outputFile.write("BEST!\nscore: "+str(best_score)+"\tthreshold: "+str(best_child)+"\n")
+			####
+			
+			###THE STUPID RED OPTIMIZATION BECAUSE STUPID HUE IS CONTINUOUS LIKE ANGLES< SO RED NEEDS TO BE SPECIAL> UGH>>>>
+
+			
+			iterations = 0
+			stale_gen = 0
+			seed1 = self.red1_hsv
+			seed2 = self.red2_hsv
+			sigma = self.SIGMA
+			##Best initializations...
+			thr_img1 = cv2.inRange(src = ycrcb_img, \
+									lowerb = seed1.lowerb, \
+									upperb = seed1.upperb )
+			thr_img2 = cv2.inRange(src = ycrcb_img, \
+									lowerb = seed2.lowerb, \
+									upperb = seed2.upperb )
+			thr_img = cv2.bitwise_or(thr_img1,thr_img2)
+			best_score = self.findFitness(thr_img,self.correct_id[seed1.name])
+			best_child1 = seed1
+			best_child2 = seed2
+			outputFile.write("\n\nSeed\t"+str(best_score)+"\t"+str(best_child1)+str(best_child2)+"\n")
+			while( iterations < self.MAX_ITER and best_score >= self.MIN_FITNESS ):
+				children1 = self.generateThresholdValues(seed1,sigma)
+				children2 = self.generateThresholdValues(seed2,sigma)
+				fitness_scores = []
+				for (i,(child1,child2)) in enumerate(zip(children1,children2)):
+					thr_img1 = cv2.inRange(src = ycrcb_img, \
+											lowerb = child1.lowerb, \
+											upperb = child1.upperb )
+					thr_img2 = cv2.inRange(src = ycrcb_img, \
+											lowerb = child2.lowerb, \
+											upperb = child2.upperb )
+					thr_img = cv2.bitwise_or(thr_img1,thr_img2)
+					fitness = self.findFitness(thr_img,self.correct_id[child1.name])
+					fitness_scores.append( (fitness,i) )
+				####
+				(gen_score,index) = min(fitness_scores)
+				iterations = iterations +1
+
+				if( gen_score < best_score ):
+					best_score = gen_score
+					best_child1 = seed1
+					best_child2 = seed2
+					seed1 = children1[index]
+					seed2 = children2[index]
+					stale_gen = 0
+					outputFile.write(str(iterations)+"\t"+str(gen_score)+"\t"+str(seed1)+str(seed2)+"\n")
+				else:
+					stale_gen = stale_gen + 1
+
+					if( stale_gen > 20 ):
+						sigma = sigma + 1
+						outputFile.write("Stale generation:\t"+str(iterations)+"\tsigma: "+str(sigma)+"\n")
+						stale_gen = 0
+					####					
+				####
+				if( self.watch == True ):
+					cv2.waitKey(100)
+				####
+			####
+			outputFile.write("BEST!\nscore: "+str(best_score)+"\tthreshold: "+str(best_child1)+str(best_child2)+"\n")
+			
+			
+			
+			
+
+
 		except:
 			outputFile.close()
 			raise
@@ -197,22 +323,22 @@ class BlockFinder():
 		outputFile.close()
 	####
 
-	def generateThresholdValues(self,seed):
+	def generateThresholdValues(self,seed,sigma):
 		"""
 		Takes in a Threshold object and generates N similar Threshold objects
 		"""
 		children = []
 		for i in xrange(self.N):
 
-			lowerb = self.random_thresh(seed.lowerb)
-			upperb = self.random_thresh(seed.upperb)
+			lowerb = self.random_thresh(seed.lowerb,sigma)
+			upperb = self.random_thresh(seed.upperb,sigma)
 			children.append( Threshold( seed.name,seed.color,lowerb,upperb))
 		####
 		return children
 	####
 
-	def random_thresh(self,t):
-		tmp = t+np.array([gauss(0,self.SIGMA),gauss(0,self.SIGMA),gauss(0,self.SIGMA)])
+	def random_thresh(self,t,sigma):
+		tmp = t+np.array([gauss(0,sigma),gauss(0,sigma),gauss(0,sigma)])
 		tmp[tmp>255]=255
 		tmp[tmp<0]=0
 		return np.array(tmp,np.uint8)
